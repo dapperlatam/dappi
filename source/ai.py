4abe75b7ec76f9b53ece3ae830fbccaa54083c9e
Pablo Manrique <pablomanriqueo12@gmail.com>
2024-07-30 13:56:34 -0500

    cambios de foormato de la base de datos y se grega self query

--- source/ai.py
+++ source/ai.py
@@ -4,6 +4,8 @@ from langchain_community.vectorstores import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from bs4 import BeautifulSoup
 from langchain import hub
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
 from langchain_community.vectorstores import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
@@ -16,9 +18,15 @@ import psycopg2
 import pandas as pd
 from datetime import datetime
 import os
+from datetime import datetime
 from dotenv import load_dotenv
+import time
 
 current_datetime = datetime.utcnow()
+today = datetime.now()
+
+# Convert to string format
+today_str = today.strftime("%Y-%m-%d")
 import re
 
 def string_to_dict_list(s):
@@ -97,16 +105,102 @@ def get_base_total(relevantes,tiempo=None):
         update_df["link"]=update_df.apply(lambda x: f"""/dashboard/{x["fuente"]}/{x["id"]}""",axis=1)
         update_df["summary"] = update_df.apply(lambda x: str(x["summary"]).encode('utf-8', 'ignore').decode('utf-8') if x["summary"] else "",axis=1)
         update_df["id"] = update_df.apply(lambda x: x["id"], axis=1)
-        update_df=update_df[['update_at',"summary",'content','link','fuente',"title", "id"]].copy()
-        update_df['update_at']=update_df['update_at'].dt.strftime('%Y-%m-%d %H:%M:%S')
+        update_df=update_df[['update_at',"summary",'content','link','fuente',"title", "id","ciudad","componente","sector","image","mes","dia","año"]].copy()
+        update_df.fillna(" ",inplace=True)
         return(update_df)
 
 #Permite hacer una busqueda espesifica de una base de dapper
 def busqueda_de_base(base,tiempo=None):
+    print(base)
+    extras=["ciudad","componente","sector"]
+    root="_".join(base.split("_")[1:])
+    last=base.split("_")[-1]
+    query=f"""
+    select {base}.id as id, {base}.created_at as creacion, {base}.update_at, {base}.title, {base}.summary, content,ARRAY_AGG(locations_city.name) as ciudad, ARRAY_AGG(component_components.name) as componente, ARRAY_AGG(sector_sector.name) as sector, {base}.image from 
+                            {base} 
+                        left join
+                        dapper_{root}_component
+                        on {base}.id = dapper_{root}_component.{last}_id
+                        left join
+                        component_components
+                        on component_components.id=dapper_{root}_component.components_id
+                        left join
+                        dapper_{root}_cities
+                        on {base}.id = dapper_{root}_cities.{last}_id
+                        left join 
+                        locations_city
+                        on locations_city.id=dapper_{root}_cities.city_id
+                        left join
+                        dapper_{root}_sectors
+                        on {base}.id = dapper_{root}_sectors.{last}_id
+                        left join
+                        sector_sector
+                        on sector_sector.id=dapper_{root}_sectors.sector_id
+                        --where_clause
+                        group by {base}.id, {base}.created_at, {base}.update_at, {base}.title, {base}.summary, content;"""
+    if root=="abc_abc":
+        query=f""" select {base}.id as id, {base}.created_at as creacion, {base}.update_at, {base}.title, body as content, ARRAY_AGG(component_components.name) as componente, ARRAY_AGG(sector_sector.name) as sector from 
+                        {base} 
+                        left join
+                        dapper_{root}_component
+                        on {base}.id = dapper_{root}_component.{last}_id
+                        left join
+                        component_components
+                        on component_components.id=dapper_{root}_component.components_id
+                        left join
+                        dapper_{root}_sectors
+                        on {base}.id = dapper_{root}_sectors.{last}_id
+                        left join
+                        sector_sector
+                        on sector_sector.id=dapper_{root}_sectors.sector_id
+                        --where_clause
+                        group by {base}.id, {base}.created_at, {base}.update_at, {base}.title,content;"""
+    elif base=="dapper_listening_content":
+        query=f"""select {base}.id as id, {base}.created_at as creacion, {base}.summary,{base}.update_at, {base}.title, {base}.body as content,ARRAY_AGG(locations_city.name) as ciudad, ARRAY_AGG(component_components.name) as componente, thumbnail as image from 
+                                {base} 
+                        left join
+                        dapper_{root}_component
+                        on {base}.id = dapper_{root}_component.{last}_id
+                        left join
+                        component_components
+                        on component_components.id=dapper_{root}_component.components_id
+                        left join
+                        dapper_{root}_cities
+                        on {base}.id = dapper_{root}_cities.{last}_id
+                        left join 
+                        locations_city
+                        on locations_city.id=dapper_{root}_cities.city_id
+                        --where_clause
+                        group by {base}.id, {base}.summary, {base}.created_at, {base}.update_at, {base}.title,content;"""
+    elif last=="news":
+        query=f"""select {base}.id as id, {base}.created_at as creacion, {base}.update_at, {base}.title, {base}.content,ARRAY_AGG(locations_city.name) as ciudad, ARRAY_AGG(component_components.name) as componente, ARRAY_AGG(sector_sector.name) as sector, {base}.image from 
+                        {base} 
+                        left join
+                        dapper_{root}_component
+                        on {base}.id = dapper_{root}_component.{last}_id
+                        left join
+                        component_components
+                        on component_components.id=dapper_{root}_component.components_id
+                        left join
+                        dapper_{root}_cities
+                        on {base}.id = dapper_{root}_cities.{last}_id
+                        left join 
+                        locations_city
+                        on locations_city.id=dapper_{root}_cities.city_id
+                        left join
+                        dapper_{root}_sectors
+                        on {base}.id = dapper_{root}_sectors.{last}_id
+                        left join
+                        sector_sector
+                        on sector_sector.id=dapper_{root}_sectors.sector_id
+                    --where_clause
+                    group by {base}.id, {base}.created_at, {base}.update_at, {base}.title, content;"""
+
     if tiempo==None:
-        sql_query = f"SELECT * FROM {base}"
+        sql_query = query
     else:
-        sql_query = f"SELECT * FROM {base} where update_at>'{tiempo}';"
+        tiempo=datetime.fromtimestamp(tiempo)
+        sql_query = query.replace("--where_clause",f"where update_at>'{tiempo}'")
     connection = psycopg2.connect(
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USERNAME"),
@@ -131,26 +225,83 @@ def busqueda_de_base(base,tiempo=None):
         # Close the connection
         connection.close()
     result_df=pd.DataFrame(result,columns=columns)
+    for columna in extras:
+        try:
+            result_df[columna]=result_df[columna].apply(lambda x: list(set([str(elemento) for elemento in x if elemento])))
+        except:
+            result_df["ciudad"]=[[] for _ in range(len(result_df))]
+    result_df["ciudad"]=result_df["ciudad"].apply(lambda x:[]  if len(x)==6 else x)
+    for columna in extras:
+        try:
+            result_df[columna]=result_df[columna].apply(lambda x: "-".join(x))
+        except:
+            pass
     result_df.rename(columns={"body":"content"},inplace=True)
-    result_df["content"]=result_df["content"].apply(lambda x: BeautifulSoup(x.encode('utf-8', 'ignore').decode('utf-8'), 'html.parser').text.replace("\n","").replace("\xa0",""))
-    print(len(result_df))
-    return(result_df)       
+    result_df["content"]=result_df["content"].apply(lambda x: BeautifulSoup(x.encode('utf-8', 'ignore').decode('utf-8'), 'html.parser').text.replace("\n"," ").replace("\xa0"," "))
+    result_df["update_at"] = pd.to_datetime(result_df["update_at"])
+# Crear las columnas de mes, día y año
+    result_df["mes"] = result_df["update_at"].dt.month
+    result_df["dia"] = result_df["update_at"].dt.day
+    result_df["año"] = result_df["update_at"].dt.year
+    result_df["update_at"]=result_df["update_at"].apply(pd.Timestamp).astype('int64') // 10**9
+    result_df["creacion"]=pd.to_datetime(result_df["creacion"]).apply(pd.Timestamp).astype('int64') // 10**9
+    return(result_df)
 
 
-
+hoy=unix_timestamp = int(time.time())
 #se definne el retriver apartir del vectorstore
+metadata_field_info = [
+    AttributeInfo(
+        name="Componente",
+        description="el tipo de contenido de la siguiente lista [Ciudades, PolifonÍA, Económico, Regulación, Ambiental-Sostenibilidad], unicamente estas opciones, cada una representa un tipo de contenido Ciudades contenido local de ciudades Economico contenido economico, PolifonÍA contenido de redes sociales y Ambiental-Sostenibilidad contenido ambiental se puede tener multiples componentes",
+        type="string o lista de strings separados por -",
+    ),
+    AttributeInfo(
+        name="Sector",
+        description="el sector economico del que trata el texto",
+        type="string",
+    ),
+    AttributeInfo(
+        name="mes",
+        description=f"mes de publicacion del contenido",
+        type="Int",
+    ),
+    AttributeInfo(
+        name="dia",
+        description=f"dia publicacion del contenido",
+        type="Int",
+    ),
+    AttributeInfo(
+        name="año",
+        description=f"mes de publicacion del contenido",
+        type="Int",
+    )
+]
 
-retriever = vectorstore.as_retriever()
+document_content_description = f"Contenido de la pagina web sobre regulacion economia y varios, la fecha de hoy es {today_str} "
+
+#retriever = vectorstore.as_retriever()
 prompt = hub.pull("rlm/rag-prompt")
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+retriever = SelfQueryRetriever.from_llm(
+    llm,
+    vectorstore,
+    document_content_description,
+    metadata_field_info,
+    verbose=True,
+    fix_invalid=True,
+    use_original_query=True
+)
 
+retriver2=vectorstore.as_retriever()
 
 #Se define el prompt
+
 
 prompt.messages[0].prompt.template="""Eres un asistente para tareas de pregunta-respuesta que usa el contenido de la plataforma DAPPER.
  Utiliza los siguientes fragmentos de contexto recuperado para responder la pregunta. Si no sabes la respuesta, simplemente di que no lo sabes.
    responde en maximo un parrafo de 70 palabras, al final de cada frase(punto) reponde entre corchetes el id de la fuente que usaste para responder esa frase ej dapper es lo mejor[1],
-     en caso de considerar la informacion insuficeinte responder que no hay infomracion sin citar
+     en caso de considerar la informacion insuficeinte o de no recibir informacion responder que no hay infomracion sin citar, porfavor solo responder con la informacpin proveida en contexto, si el contexto esta vacio reponder que no hay informacion
      \nPregunta: {question} \nContexto: {context} \nRespuesta:"""
 
 
@@ -173,6 +324,9 @@ rag_chain_from_docs = (
 rag_chain_with_source = RunnableParallel(
     {"context": retriever, "question": RunnablePassthrough()}
 ).assign(answer=rag_chain_from_docs)
+rag_chain_with_source2 = RunnableParallel(
+    {"context": retriver2, "question": RunnablePassthrough()}
+).assign(answer=rag_chain_from_docs)
 
 def dict_in_list(dict_list, dict_item):
     for d in dict_list:
@@ -181,7 +335,13 @@ def dict_in_list(dict_list, dict_item):
     return False
 
 def respondedor(pregunta):
-    respuesta=rag_chain_with_source.invoke(pregunta)
+    try:
+        respuesta=rag_chain_with_source.invoke(pregunta)
+    except:
+        print("no funciono el filtro se manda sin filtro")
+        respuesta=rag_chain_with_source2.invoke(pregunta)
+
+        
     contexto=respuesta["context"].copy()
     parseado=string_to_dict_list(respuesta["answer"])
     contexto_new=[]
@@ -213,7 +373,7 @@ def actualizar(all=False):
     if all or len(current_df)==0:
         final=None
     else:
-        final=pd.to_datetime(current_df["update_at"]).max().strftime('%Y-%m-%d %H:%M:%S')
+        final=current_df["update_at"].max()
     print(final)
     update=get_base_total(bases_relevantes,tiempo=final)
     if not update.empty:
@@ -229,7 +389,7 @@ def actualizar(all=False):
         ids= [split.metadata["link"] for split in splits]
         contents=[split.page_content for split in splits]
         metadatas=[split.metadata for split in splits]
-        print('metadatas', metadatas)
-        print('contents', contents)
+        print('metadatas', metadatas[:2])
+        print('contents', contents[:2])
         ids=add_position_to_duplicates(ids)
         collection.add(ids=ids,documents=contents,metadatas=metadatas)
--- source/server.py
+++ source/server.py
@@ -8,7 +8,10 @@ import ai as ai
 app = FastAPI()
 
 origins = [
-    "*"
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
 ]
 
 app.add_middleware(
